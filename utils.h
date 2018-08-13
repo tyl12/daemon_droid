1367b630ff70cece3155deeabdc9b09d64b412b8
Teng Yiliang <yiliang.teng@weimob.com>
2018-08-13 20:02:14 +0800

    update utils.h & error.h
    Change-Id: I582210f80ad1f4e182632cc83f67f6487797b710
    Signed-off-by: Teng Yiliang <yiliang.teng@weimob.com>

--- utils.h
+++ utils.h
@@ -2,8 +2,36 @@
 // Created by david on 7/3/18.
 //
 
-#ifndef C4FUN_ERROR_H
-#define C4FUN_ERROR_H
-void err_quit(const char *fmt, ...);
-void err_sys(const char *fmt, ...);
-#endif //C4FUN_ERROR_H
+#ifndef C4FUN_UTILS_H
+#define C4FUN_UTILS_H
+
+#include <stdbool.h>
+#include <signal.h>
+#include <android/log.h>
+
+#define      LOG_TAG "XIAOMENG.DAEMON"
+#define   LOG_I(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define   LOG_E(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+
+
+#define BUF_SIZE 256
+
+typedef struct
+{
+    char name[256];
+    bool is_alive;
+    pid_t pid;
+} proc_info;
+
+void pr_exit(int state);
+void check_apk(char *apk);
+int link_data();
+void list2vec(char *list, char **vec);
+proc_info check_proc(const char *proc_name);
+void get_pid(pid_t *pid, const char *proc_name);
+void get_time(char *time_s);
+size_t trim(char *out, const char *src);
+void kill_proc(const char *name);
+void lowerstr(char *str);
+void getwchan(char *wchan, pid_t pid); /* waitting channel */
+#endif //C4FUN_UTILS_H
