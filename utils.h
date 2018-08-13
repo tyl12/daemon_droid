f61fa4473042434f6b83fe8a491207d3ff31feb7
Teng Yiliang <yiliang.teng@weimob.com>
2018-08-13 19:53:12 +0800

    add missing utils.h
    Change-Id: Ie1ffc115008ab64952c5c4b8d280d37fb3162874
    Signed-off-by: Teng Yiliang <yiliang.teng@weimob.com>

---
+++ utils.h
@@ -0,0 +1,9 @@
+//
+// Created by david on 7/3/18.
+//
+
+#ifndef C4FUN_ERROR_H
+#define C4FUN_ERROR_H
+void err_quit(const char *fmt, ...);
+void err_sys(const char *fmt, ...);
+#endif //C4FUN_ERROR_H
