0cef8e788c57b378909f2e9de7870e6347c5de0e
Teng Yiliang <yiliang.teng@weimob.com>
2018-09-21 20:35:41 +0800

    clean codebase.
    Change-Id: I45e3aacd65dd6ef9c27a77af5537064c47f4dee2
    Signed-off-by: Teng Yiliang <yiliang.teng@weimob.com>

--- error.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by david on 7/3/18.
-//
-
-#ifndef C4FUN_ERROR_H
-#define C4FUN_ERROR_H
-void err_quit(const char *fmt, ...);
-void err_sys(const char *fmt, ...);
-#endif //C4FUN_ERROR_H
