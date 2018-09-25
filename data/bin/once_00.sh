02b0d4add914067fed18a3d278b34d7cdee39e3b
Teng Yiliang <yiliang.teng@weimob.com>
2018-09-25 11:22:41 +0800

    clean codebase, remove data&sdcard dir.
    Change-Id: I630dea12535d8b561a87de0caa7e6b565587af33
    Signed-off-by: Teng Yiliang <yiliang.teng@weimob.com>

--- data/bin/once_00.sh
+++
@@ -1,5 +0,0 @@
-. $ENV_XIAOMENG_SCRIPT_DIR/utils.sh
-echo "=====>$0 begin @ `get_time`"
-TAG="$0 : "
-
-echo "=====>$0 end @ `get_time`"
--- data/bin/period_00_rssh.sh
+++
@@ -1,9 +0,0 @@
-. $ENV_XIAOMENG_SCRIPT_DIR/utils.sh
-echo "=====>$0 begin @ `get_time`"
-TAG="$0 : "
-
-launch_ssh
-
-sleep 60
-
-echo "=====>$0 end @ `get_time`"
