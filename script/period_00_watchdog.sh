6f072200de0ea71be66620f3a26740d1a0cae4f8
Teng Yiliang <ylteng@outlook.com>
2018-12-21 09:17:34 +0800

    update.
    Change-Id: Iab3eebd488b9911656a32b87c5c3cd0b4423eacf
    Signed-off-by: Teng Yiliang <ylteng@outlook.com>

--- script/period_00_watchdog.sh
+++ script/period_00_watchdog.sh
@@ -4,4 +4,16 @@ TAG="$0 : "
 
 sleep 60
 
+pinfo=`ps|grep -i client_droid`
+pinfo=`echo $pinifo`
+if [ "$pinfo" == "" ]; then
+    echo "client_droid not launch, skip monitor this time"
+    sleep 20
+else
+    echo "client_droid alive, check status"
+    if [ -f /data/local/tmp/status ]; then
+        echo "
+
+fi
+
 echo "=====>$0 end @ `get_time`"
